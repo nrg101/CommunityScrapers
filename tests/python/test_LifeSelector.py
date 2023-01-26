b0af04c6bbb8493c69e2d06bf6bf1b0349b9e762
nrg101 <nrg101@macbook>
2023-01-26 02:28:46 +0000

    fix wording

--- tests/python/test_LifeSelector.py
+++ tests/python/test_LifeSelector.py
@@ -5,10 +5,10 @@ import unittest
 # to import from a parent directory we need to add that directory to the system path
 # get current script directory
 current_script_directory = os.path.dirname(os.path.realpath(__file__))
-# grandparent directory (should be the scrapers one)
+# grandparent directory (should be the repo root folder)
 grandparent = os.path.dirname(os.path.dirname(current_script_directory))
 scrapers_directory = os.path.join(grandparent, 'scrapers')
-# add grandparent dir to sys path so that we can import py_common from there
+# add scrapers dir to sys path so that we can import py_common from there
 print(scrapers_directory)
 sys.path.append(scrapers_directory)
 
